6106c7f3924dbbcbe938ef7810cec43cb31bb752
Colin McCullough <colin.mccullo@gmail.com>
2017-07-26 15:16:40 -0400

    updating of stores added

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -32,9 +32,34 @@ exports.addStore = (req,res) => {
 exports.createStore = async (req, res) => {
 	const store = await (new Store(req.body)).save();
 	console.log("store has been saved");
-	// coonect-flash is a middleware imported in app.js
+	// connect-flash is a middleware imported in app.js
 	req.flash("success", `Successfully added ${store.name}. Care to leave a review?`);
 	res.redirect(`/store/${store.slug}`);
+}
+
+exports.getStores = async (req, res) => {
+	// 1. Query the databases for a list of all the stores
+	const stores = await Store.find();
+	res.render("stores", { title: "Stores", stores: stores });
+}
+
+exports.editStore = async (req, res) => {
+	// 1. Find the store givin the ID
+	const store = await Store.findOne({ _id: req.params.id });
+	// 2. Confirm they are the owner of the store
+	// TODO
+	res.render("editStore", { title: `Edit ${store.name}`, store: store });
+}
+
+exports.updateStore = async (req, res) => {
+	// findOneAndUpdate is a method from mongoDb
+	const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+		new: true, // returns the new store with updates and not the old one
+		runValidators: true // validate the updated store against the storeSchema
+	}).exec();
+
+	req.flash("success", `Sucessfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store</a>`);
+	res.redirect(`/stores/${store._id}/edit`);
 }
 
 exports.homePage = (req, res) => {
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -5,11 +5,15 @@ const router = express.Router();
 const storeController = require("../controllers/storeController");
 const { catchErrors } = require("../handlers/errorHandlers");
 
+// USING ASYNC AWAIT, wrapped in catchErrors function
+router.get("/doge/", catchErrors(storeController.dogePage));
+
 router.get("/add", storeController.addStore);
+router.post("/add", catchErrors(storeController.createStore)); 
+router.post("/add/:id", catchErrors(storeController.updateStore));
 
-router.post("/add", catchErrors(storeController.createStore)); // USING ASYNC AWAIT, wrapped in catchErrors function
-
-router.get("/doge/", storeController.dogePage);
+router.get("/stores", catchErrors(storeController.getStores));
+router.get("/stores/:id/edit", catchErrors(storeController.editStore));
 
 router.get("/", storeController.homePage); // Applying Middleware
 
